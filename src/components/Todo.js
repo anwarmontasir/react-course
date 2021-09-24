07872ddeb11338ecdbcc84a115b59429821aa4f8
anwarmontasir <anwarmontasir@gmail.com>
2021-09-23 17:53:09 -0700

    close modal functions

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -21,7 +21,7 @@ function Todo(props) {
           Delete
         </button>
       </div>
-      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />}
       {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
